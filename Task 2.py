6d21befebebb7bf40192feec2f9086f492e18602
ZiadAhmed22 <114878192+ZiadAhmed22@users.noreply.github.com>
2022-10-03 14:21:43 -0700

    Add files via upload

---
+++ Task 2.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn import datasets
+
+
+# In[16]:
+
+
+iris = datasets.load_iris()
+df = pd.DataFrame(iris.data, columns = iris.feature_names)
+df
+
+
+# In[17]:
+
+
+x = df.iloc[:, [0, 1, 2, 3]].values
+from sklearn.cluster import KMeans
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters = i, init = 'k-means++', 
+                    max_iter = 300, n_init = 10, random_state = 0)
+    kmeans.fit(x)
+    wcss.append(kmeans.inertia_)
+
+
+# In[18]:
+
+
+plt.plot(range(1, 11), wcss)
+plt.title('The elbow method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS') # Within cluster sum of squares
+plt.show()
+
+
+# In[19]:
+
+
+kmeans = KMeans(n_clusters = 3, init = 'k-means++',
+                max_iter = 300, n_init = 10, random_state = 0)
+y_kmeans = kmeans.fit_predict(x)
+
+
+# In[20]:
+
+
+plt.scatter(x[y_kmeans == 0, 0], x[y_kmeans == 0, 1], 
+            s = 100, c = 'red', label = 'Iris-setosa')
+plt.scatter(x[y_kmeans == 1, 0], x[y_kmeans == 1, 1], 
+            s = 100, c = 'blue', label = 'Iris-versicolour')
+plt.scatter(x[y_kmeans == 2, 0], x[y_kmeans == 2, 1],
+            s = 100, c = 'green', label = 'Iris-virginica')
+
+
+# In[21]:
+
+
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:,1], 
+            s = 100, c = 'yellow', label = 'Centroids')
+
+plt.legend()# Plotting the centroids of the clusters
+
+
+# In[22]:
+
+
+plt.scatter(x[y_kmeans == 0, 0], x[y_kmeans == 0, 1], 
+            s = 100, c = 'red', label = 'Iris-setosa')
+plt.scatter(x[y_kmeans == 1, 0], x[y_kmeans == 1, 1], 
+            s = 100, c = 'blue', label = 'Iris-versicolour')
+plt.scatter(x[y_kmeans == 2, 0], x[y_kmeans == 2, 1],
+            s = 100, c = 'green', label = 'Iris-virginica')
+
+# Plotting centroids of the clusters
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:,1], 
+            s = 100, c = 'yellow', label = 'Centroids')
+
+plt.legend()
+
+
+# In[ ]:
+
+
+
+
